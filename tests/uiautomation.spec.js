165e31edcd95f6eba1ca953254580f3f5994d3ca
Utkarsh630 <shivamsr1998@gmail.com>
2025-01-01 22:59:30 +0530

    login test case - with page.locator() only

--- tests/uiautomation.spec.js
+++ tests/uiautomation.spec.js
@@ -1,12 +1,12 @@
-const {test} = require('@playwright/test');
+const {test, expect} = require('@playwright/test');
 
-test.only('First Test Case',async({page})=>{
+test.only('Registration Test Case',async({page})=>{
 
     await page.goto('https://rahulshettyacademy.com/client');
     await page.getByText('Register here').click();
     await page.getByPlaceholder('First Name').fill('raju');
     await page.getByPlaceholder('Last Name').fill('sin');
-    await page.getByPlaceholder('email@example.com').fill('rajusin675@getMaxListeners.com');
+    await page.getByPlaceholder('email@example.com').fill('rajusin66775@gmail.com');
     await page.getByPlaceholder('enter your number').fill('9867561234');
     await page.locator('[formcontrolname="occupation"]').selectOption('1: Doctor');
     await page.locator('input[value="Male"]').check();
@@ -14,5 +14,17 @@ test.only('First Test Case',async({page})=>{
     await page.locator("#confirmPassword").fill('Abc@12345');
     await page.locator('[formcontrolname="required"]').check();
     await page.getByRole('button',{name: 'Register'}).click();
+
+    await expect(page.locator('.headcolor')).toHaveText('Account Created Successfully');
+    await expect(page.getByRole('button',{name: 'Login'})).toBeVisible();
 });
+
+test('Login Test Case',async({page})=>{
+    await page.goto('https://rahulshettyacademy.com/client');
+    await page.locator('#userEmail').fill('rajusin675@gmail.com');
+    await page.locator('userPassword').fill('Abc@12345');
+    await page.locator('#login').click();
+    await expect(page.locator('#toast-container div')).toHaveText('Login Successful');
+});
+
 
